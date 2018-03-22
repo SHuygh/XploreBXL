391749a6a04e25dd0f57ef1b0807b8109bfd75a4
SHuygh <stijn.huygh@gmail.com>
2018-03-22 14:10:27 +0100

    Fixed List + load in saved images

--- app/src/main/java/be/ehb/xplorebxl/View/Activities/MainActivity.java
+++ app/src/main/java/be/ehb/xplorebxl/View/Activities/MainActivity.java
@@ -1,16 +1,24 @@
 package be.ehb.xplorebxl.View.Activities;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Message;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -38,6 +46,7 @@ import com.squareup.picasso.Target;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.Provider;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -68,10 +77,11 @@ public class MainActivity extends AppCompatActivity
 
     public GoogleMap map;
     private HashMap<Marker, Object> objectLinkedToMarker;
-    private  Button btnCloseExtraFrag;
+    private Button btnCloseExtraFrag;
     private ArrayList<Target> targetComicList = new ArrayList<>();
     private ArrayList<Target> targetStreetartList = new ArrayList<>();
-
+    LocationManager locationManager;
+    LocationListener locationListener;
 
 
     @Override
@@ -112,14 +122,74 @@ public class MainActivity extends AppCompatActivity
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-        if(sharedPreferences.getBoolean("AppHasDownloadedDataBefore", false)){
-        }else{
+        if (sharedPreferences.getBoolean("AppHasDownloadedDataBefore", false)) {
+        } else {
             downloadData();
         }
 
+        locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
 
 
 
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Museum museum = LandMarksDatabase.getInstance(getApplicationContext()).getMuseums().get(0);
+                if(museum != null) {
+                    Location locationMuseum = new Location("locationMuseum");
+
+                    locationMuseum.setLatitude(museum.getCoordX());
+                    locationMuseum.setLongitude(museum.getCoordY());
+                    Toast.makeText(getApplicationContext(),
+                            "Distance to " + museum.getName() + " is equals to " + location.distanceTo(locationMuseum) + " m", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "No GPS LOCATION", Toast.LENGTH_LONG).show();
+                        return;
+                    } else {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+                    }
+                }
+        }
     }
 
     @Override
@@ -167,7 +237,7 @@ public class MainActivity extends AppCompatActivity
             findViewById(R.id.detail_frag_container).setVisibility(View.GONE);
             btnCloseExtraFrag.setVisibility(View.GONE);
 
-        } else if(id == R.id.nav_update){
+        } else if (id == R.id.nav_update) {
             this.downloadData();
             Toast.makeText(this, "Data updated", Toast.LENGTH_LONG).show();
         }
@@ -182,6 +252,7 @@ public class MainActivity extends AppCompatActivity
         map = googleMap;
 
         setupMap();
+
     }
 
     private void setupMap() {
@@ -189,6 +260,12 @@ public class MainActivity extends AppCompatActivity
         //map.setOnInfoWindowClickListener(this);
         map.setOnMarkerClickListener(this);
         updateCamera();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        }else {
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setMyLocationButtonEnabled(true);
+        }
+
     }
 
     public void drawMarkers() {
@@ -297,7 +374,7 @@ public class MainActivity extends AppCompatActivity
         backGroundThread.start();
 
     }
-
+//BASED ON http://www.codexpedia.com/android/android-download-and-save-image-through-picasso/
     public void downloadImgs(List<String> imgs, String type) {
         ArrayList<String> imgUrlList = (ArrayList<String>) imgs;
 
