1f23d5ab2446b705b77fa8e3ce54b673dc829e3c
daandemees <daan.demeulemeester@gmail.com>
2018-03-26 16:07:59 +0200

    Comic POJO, DAO, DB, REST, MAIN

---
+++ app/src/main/java/be/ehb/xplorebxl/Utils/Adapter/FabAdapter.java
@@ -0,0 +1,25 @@
+package be.ehb.xplorebxl.Utils.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Millmaster on 26/03/2018.
+ */
+
+class FabAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.CustomViewHolder> {
+    @Override
+    public MyRecyclerViewAdapter.CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MyRecyclerViewAdapter.CustomViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/be/ehb/xplorebxl/View/Fragments/FabFragment.java
@@ -0,0 +1,16 @@
+package be.ehb.xplorebxl.View.Fragments;
+
+import android.support.v7.widget.RecyclerView;
+
+import be.ehb.xplorebxl.Utils.Adapter.MyRecyclerViewAdapter;
+
+/**
+ * Created by Millmaster on 26/03/2018.
+ */
+
+public class FabFragment {
+
+    private RecyclerView mRecyclerView;
+    private MyRecyclerViewAdapter adapter;
+
+}
